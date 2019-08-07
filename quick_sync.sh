09c497ecc9d2adad5e8f9a02284c0f049741e467
Pubfred <zeonmymail@gmail.com>
2019-08-07 15:48:48 +0200

    Update quick_sync.sh

--- quick_sync.sh
+++ quick_sync.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash -i
 # Zeon Quick sync Script V1.2 for Ubuntu 18.04 LTS
-# (c) 2019 by Pubfred for ZEON 
+# (c) 2019 by Pubfred for ZEON
 #
 # Script will attempt to download zeon_blocks_385000.tar.gz
 # from Google Drive and copy blocks in ~/.zeon folder .
@@ -11,23 +11,20 @@
 
 
 
-cat <<EOF >> ~/.bash_aliases
-
 function gdrive_download () {
-CONFIRM=\$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=\$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=\$CONFIRM&id=\$1" -O \$2
+CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
 rm -rf /tmp/cookies.txt
 }
 
-EOF
 
-exec bash
+zeon-cli stop
 
-zeon-cli stop 
+cd ~/.zeon
 
-cd ~/.zeon 
- 
 gdrive_download 1cy4kPV1_seCXTTPilHZ5JOLzb1cz2YQn zeon_blocks_385000.tar.gz
 
-tar xvf  zeon_blocks_385000.tar.gz
+sudo tar -xvf  ~/.zeon/zeon_blocks_385000.tar.gz
+
+
 
