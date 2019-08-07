45de8dfb9b96f9000da90fd01d03c7ca7f6d6880
Pubfred <zeonmymail@gmail.com>
2019-08-07 14:24:22 +0200

    Update quick_sync.sh

--- quick_sync.sh
+++ quick_sync.sh
@@ -1,11 +1,33 @@
+#!/bin/bash
+# Zeon Quick sync Script V1.2 for Ubuntu 18.04 LTS
+# (c) 2019 by Pubfred for ZEON 
+#
+# Script will attempt to download zeon_blocks_385000.tar.gz
+# from Google Drive and copy blocks in ~/.zeon folder .
+#
+# Usage:
+# bash quick_sync.sh
+#
 
 
 
+cat <<EOF > ~/.bash_aliases
 
+function gdrive_download () {
+CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+rm -rf /tmp/cookies.txt
+}
 
-
-
-cat <<EOF > ~/.moondexcore/moondex.conf
-rpcuser=$rpcuser
-rpcpassword=$rpcpassword
 EOF
+
+bash exec
+
+zeon-cli stop 
+
+cd ~/.zeon 
+ 
+gdrive_download 1cy4kPV1_seCXTTPilHZ5JOLzb1cz2YQn zeon_blocks_385000.tar.gz
+
+tar xvf  zeon_blocks_385000.tar.gz
+
